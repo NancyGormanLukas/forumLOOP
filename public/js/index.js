ed9a7997609b58ade5f263bef1925cfbfbaec5af
Nancy Gorman Lukas <nlukas13@optonline.net>
2016-10-18 19:18:30 -0400

    firebase update

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,28 @@
-var ref = new Firebase("https://theloop-85f4d.firebaseio.com/");
+//var ref = new Firebase("https://theloop-85f4d.firebaseio.com/");
 
-
+var provider = new firebase.auth.GithubAuthProvider();
 $(document).ready(function() {
 
-	var fireBaseSession = localStorage.getItem('firebase:session::theLoop');
-	var user;
+	//var fireBaseSession = localStorage.getItem('firebase:session::theLoop');
+	//var user;
 
-	if (fireBaseSession) {
-	    user = JSON.parse(fireBaseSession);
-	}
+	//if (fireBaseSession) {
+	    //user = JSON.parse(fireBaseSession);
+	    firebase.auth().signInWithPopup(provider).then(function(result) {
+  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+			  var token = result.credential.accessToken;
+			  // The signed-in user info.
+			  var user = result.user;
+			  // ...
+			}).catch(function(error) {
+			  // Handle Errors here.
+			  var errorCode = error.code;
+			  var errorMessage = error.message;
+			  // The firebase.auth.AuthCredential type that was used.
+			  var credential = error.credential;
+			  // ...
+			});
+	// }
 
 	if (user && user.github) {
 
